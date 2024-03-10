a834d89122a8810d432979e089bc6fe2222d2e37
vivtej411 <vivekgobburu@gmail.com>
2024-03-10 12:26:30 +0530

    deploy modifications

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: `http://${process.env.IP_ADD}:3000`,
+    origin: `http://localhost:3000`,
   },
 });
 
